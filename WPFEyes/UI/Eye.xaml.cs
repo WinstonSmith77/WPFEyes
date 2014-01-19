cbe37ba8607cfeaf8f848da6c0a6b2ffed3a9fd4
WinstonSmith77 <matze_henning@gmx.de>
2014-01-19 21:07:29 +0100

    Works!!!

--- WPFEyes/UI/Eye.xaml.cs
+++ WPFEyes/UI/Eye.xaml.cs
@@ -13,15 +13,14 @@ namespace WPFEyes.UI
         private const int _maxOffset = 1000;
         private readonly double _extension;
         private readonly double _extensionInnerEye;
+        private readonly double _extensionBlue;
 
         public Eye()
         {
             InitializeComponent();
             DataContext = this;
 
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(50), IsEnabled = true };
-            timer.Tick += timer_Tick;
-
+            InitTimer();
 
             _extension = (double)FindResource("Extension");
             Center = new Point(_extension / 2, _extension / 2);
@@ -29,6 +28,15 @@ namespace WPFEyes.UI
             _extensionInnerEye = (double)FindResource("ExtensionInnerEye");
             OffsetXInnerEye = Center.X - _extensionInnerEye / 2;
             OffsetYInnerEye = Center.Y - _extensionInnerEye / 2;
+
+            _extensionBlue = (double)FindResource("ExtensionBlue");
+            OffsetBlue = (_extensionInnerEye - _extensionBlue) / 2;
+        }
+
+        private void InitTimer()
+        {
+            var timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(50), IsEnabled = true};
+            timer.Tick += timer_Tick;
         }
 
         void timer_Tick(object sender, EventArgs e)
@@ -42,20 +50,25 @@ namespace WPFEyes.UI
             var angle = Math.Atan2(offset.Y, offset.X);
             var length = Math.Sqrt(offset.X * offset.X + offset.Y * offset.Y);
 
-            length = Math.Min(Math.Abs(length), _maxOffset) * Math.Sign(length);
+            //  length = Math.Min(Math.Abs(length), _maxOffset) * Math.Sign(length);
 
-            length *= _extension / _maxOffset;
-           // length = 0;
+            length *= _extension / _maxOffset / 2;
 
-            offset = new Point(Math.Cos(angle) * length, Math.Sin(angle) * length);
-
-            OffsetXInnerEye = Center.X + offset.X - _extensionInnerEye / 2;
-            OffsetYInnerEye = Center.Y + offset.Y - _extensionInnerEye / 2;
+            offset = CalcPos(angle, length, _extensionInnerEye, x => OffsetXInnerEye = x, y => OffsetYInnerEye = y);
 
             Info = offset.ToString();
         }
 
+        private Point CalcPos(double angle, double length, double extension, Action<double> setX, Action<double> setY)
+        {
+            length = Math.Min(Math.Abs(length), (_extension / 2 - extension)) * Math.Sign(length);
 
+            var offset = new Point(Math.Cos(angle) * length, Math.Sin(angle) * length);
+
+            setX(Center.X + offset.X - extension / 2);
+            setY(Center.Y + offset.Y - extension / 2);
+            return offset;
+        }
 
 
         public Point Center
@@ -67,6 +80,18 @@ namespace WPFEyes.UI
         // Using a DependencyProperty as the backing store for Center.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CenterProperty =
             DependencyProperty.Register("Center", typeof(Point), typeof(Eye), new PropertyMetadata(new Point()));
+
+
+
+        public double OffsetBlue
+        {
+            get { return (double)GetValue(OffsetBlueProperty); }
+            set { SetValue(OffsetBlueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OffsetBlue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OffsetBlueProperty =
+            DependencyProperty.Register("OffsetBlue", typeof(double), typeof(Eye), new PropertyMetadata(0.0));
 
 
 
