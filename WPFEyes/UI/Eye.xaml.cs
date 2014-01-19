feaa3a76014047a1323108ad12d848fcd70e3b4d
WinstonSmith77 <matze_henning@gmx.de>
2014-01-19 20:12:04 +0100

    Next Step

--- WPFEyes/UI/Eye.xaml.cs
+++ WPFEyes/UI/Eye.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPFEyes.UI
 {
@@ -20,9 +23,48 @@ namespace WPFEyes.UI
     /// </summary>
     public partial class Eye : UserControl
     {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePositionOnScreen()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+
         public Eye()
         {
             InitializeComponent();
+            DataContext = this;
+
+            var timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(50), IsEnabled = true};
+            timer.Tick += timer_Tick;
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Info =  GetMousePositionOnScreen().ToString();
+        }
+
+        public string Info
+        {
+            get { return (string)GetValue(InfoProperty); }
+            set { SetValue(InfoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Info.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InfoProperty =
+            DependencyProperty.Register("Info", typeof(string), typeof(Eye), new PropertyMetadata(String.Empty));
+
+        
     }
 }
